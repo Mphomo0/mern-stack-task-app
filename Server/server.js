eb05ba977a969c10008a11ee40573232012b207a
mpho.moipolai <mpho.moipolai@autotrader.co.za>
2023-10-16 16:47:08 +0200

    checking something

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const cors = require('cors')
+
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
 dotenv.config()
@@ -25,10 +25,27 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-const corsOption = {
-  origin: ['https://mernstack-todo-app.onrender.com'],
-};
-app.use(cors(corsOption))
+// Set middleware of CORS 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://your-frontend.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
 
 app.use('/api/users', userRoutes)
 app.use('/api/todos', todoRoutes)
