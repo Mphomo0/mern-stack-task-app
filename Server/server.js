a19420cd3bdb371cdd49eb74e9239adae16f88d8
mpho.moipolai <mpho.moipolai@autotrader.co.za>
2023-10-16 16:48:00 +0200

    changed link

--- Server/server.js
+++ Server/server.js
@@ -29,7 +29,7 @@ app.use(cookieParser())
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://your-frontend.com"
+    "https://mernstack-todo-app.onrender.com/"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
