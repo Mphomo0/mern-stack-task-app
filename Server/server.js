df76a78fa497ee024303e252691e82be2d211b01
mpho.moipolai <mpho.moipolai@autotrader.co.za>
2023-10-16 13:36:51 +0200

    another change

--- Server/server.js
+++ Server/server.js
@@ -24,7 +24,13 @@ if (process.env.NODE_ENV === "production") {
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use(cors())
+
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+app.use(cors(corsOptions))
 
 app.use('/api/users', userRoutes)
 app.use('/api/todos', todoRoutes)
